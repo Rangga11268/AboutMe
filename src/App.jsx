8b9148277f93f6ba88603021a41506c0f1422aaa
Darell Rangga <darrelrangga@gmail.com>
2025-07-12 06:28:10 +0700

    Setengah dari navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <>
-      <h1 className="text-4xl font-bold underline">Hello wolrd</h1>
-    </>
+    <>{/* <h1 className="text-4xl font-bold underline">Hello wolrd</h1> */}</>
   );
 }
