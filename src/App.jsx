be610a1dfd9bff8904a2e2c61c7da11b2875f7de
Darell Rangga <darrelrangga@gmail.com>
2025-07-13 06:33:01 +0700

    Penambahan hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
+import DataImage from "./data";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-        reprehenderit nulla commodi voluptas. Cumque impedit, pariatur,
-        perspiciatis quos, dolor enim corporis numquam beatae saepe distinctio
-        ratione nihil minima placeat facilis. Ullam ipsum asperiores molestiae
-        repellat debitis ducimus sequi, sapiente cum, iure similique dolores
-        quos explicabo quasi doloribus? Id, consequatur beatae. Inventore
-        eligendi aliquid atque dolores quidem commodi aliquam alias incidunt?
-        Molestiae, alias dignissimos maiores adipisci distinctio ad beatae
-        deleniti, doloribus, odio at laboriosam consequatur praesentium nihil
-        possimus! Praesentium esse nostrum quod qui consequatur totam ipsum
-        corrupti ut exercitationem a hic optio, libero omnis, necessitatibus
-        possimus excepturi tenetur quaerat eius dolorem! Tempora iure sapiente
-        ipsa laboriosam rerum suscipit voluptas sequi nisi blanditiis ea, minima
-        facilis dignissimos deserunt dolore et illo magnam? Consequatur atque
-        aliquam repudiandae nobis eaque nulla reiciendis, quos ab quia iure
-        distinctio voluptatum, explicabo, aperiam ex soluta doloremque ut
-        praesentium animi eos modi obcaecati suscipit. Culpa quos maiores,
-        corrupti enim porro assumenda deleniti laborum minima quis ex cupiditate
-        facere. Temporibus cum ut dolor aperiam libero quo reprehenderit, rem
-        assumenda eos incidunt? Numquam similique eius placeat in voluptatum
-        minima voluptatem autem veniam inventore laborum, nostrum totam qui,
-        tempora eos ipsa ipsam culpa omnis sequi temporibus repudiandae
-        consequatur quia obcaecati perspiciatis!
-      </h1>
+      <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
+        <div>
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+            <img
+              src={DataImage.HeroImage}
+              alt="Hero"
+              className="w-10 rounded-md"
+            />
+            <q>Lorem ipsum dolor sit, amet consectetur 🔥</q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">
+            Hi, Saya Darell rangga
+          </h1>
+          <p className="text-base/loose mb-6 opacity-50">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos
+            doloremque sint dignissimos nisi soluta porro delectus earum quae
+            atque nam. Quisquam exercitationem optio commodi magnam veritatis
+            possimus quod ea. Est autem, impedit quos officia facere quidem sunt
+            adipisci? Magni, perspiciatis iste ipsa rerum eos distinctio
+            tenetur. Asperiores tempora quidem iste repellendus sint? Voluptas,
+            ipsam fugiat corporis inventore impedit velit mollitia repellat
+            saepe aliquam sequi voluptatem ullam id quasi! Deserunt eligendi
+            totam reiciendis numquam consequatur quae magni quisquam distinctio,
+            repellat atque repellendus ipsum incidunt, quasi dolore commodi iure
+            aspernatur voluptas culpa.
+          </p>
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a
+              className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600"
+              href="#"
+            >
+              Download CV <i className="ri-download-line ri-lg"></i>
+            </a>
+            <a
+              className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600"
+              href="#"
+            >
+              Lihat Project <i className="ri-arrow-down-line ri-lg"></i>
+            </a>
+          </div>
+        </div>
+        <img
+          src={DataImage.HeroImage}
+          alt="Hero"
+          className="w-[500px] md:ml-auto rounded-2xl"
+        />
+      </div>
     </>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-import HeroImage from "/assets/hero-img.webp";
+import HeroImage from "/assets/me3.jpeg";
 
 const Image = {
   HeroImage,
