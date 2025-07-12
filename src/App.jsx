28932dfb813a92ebd838bbe1935e2132d95bcf40
Darell Rangga <darrelrangga@gmail.com>
2025-07-13 06:07:18 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 export default function App() {
   return (
-    <>{/* <h1 className="text-4xl font-bold underline">Hello wolrd</h1> */}</>
+    <>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        reprehenderit nulla commodi voluptas. Cumque impedit, pariatur,
+        perspiciatis quos, dolor enim corporis numquam beatae saepe distinctio
+        ratione nihil minima placeat facilis. Ullam ipsum asperiores molestiae
+        repellat debitis ducimus sequi, sapiente cum, iure similique dolores
+        quos explicabo quasi doloribus? Id, consequatur beatae. Inventore
+        eligendi aliquid atque dolores quidem commodi aliquam alias incidunt?
+        Molestiae, alias dignissimos maiores adipisci distinctio ad beatae
+        deleniti, doloribus, odio at laboriosam consequatur praesentium nihil
+        possimus! Praesentium esse nostrum quod qui consequatur totam ipsum
+        corrupti ut exercitationem a hic optio, libero omnis, necessitatibus
+        possimus excepturi tenetur quaerat eius dolorem! Tempora iure sapiente
+        ipsa laboriosam rerum suscipit voluptas sequi nisi blanditiis ea, minima
+        facilis dignissimos deserunt dolore et illo magnam? Consequatur atque
+        aliquam repudiandae nobis eaque nulla reiciendis, quos ab quia iure
+        distinctio voluptatum, explicabo, aperiam ex soluta doloremque ut
+        praesentium animi eos modi obcaecati suscipit. Culpa quos maiores,
+        corrupti enim porro assumenda deleniti laborum minima quis ex cupiditate
+        facere. Temporibus cum ut dolor aperiam libero quo reprehenderit, rem
+        assumenda eos incidunt? Numquam similique eius placeat in voluptatum
+        minima voluptatem autem veniam inventore laborum, nostrum totam qui,
+        tempora eos ipsa ipsam culpa omnis sequi temporibus repudiandae
+        consequatur quia obcaecati perspiciatis!
+      </h1>
+    </>
   );
 }
